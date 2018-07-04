c056ea6f66f2ab07701847f5cb44e8678a82a105
jlq <524608951@qq.com>
2018-07-04 23:16:14 +0800

    add svg-icon

---
+++ src/components/svgIcon/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconClass"></use>
+  </svg>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'icon-svg',
+    props: {
+      svgName: {
+        type: 'string'
+      },
+      iconName: {
+        type: 'string',
+        required: true
+      }
+    },
+    computed: {
+      svgClass() {
+        if (this.svgName) {
+          return  `svg-icon  + ${this.svgName}`
+        }else{
+          return 'svg-icon'
+        }
+      },
+      iconClass() {
+        return `${this.iconName}`
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/less" lang="less" scoped>
+  .svg-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
